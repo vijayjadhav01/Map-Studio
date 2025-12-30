4899af66b9315bc378d18d550a34fe6731ead56c
vijayjadhav01 <103282797+vijayjadhav01@users.noreply.github.com>
2025-12-30 22:11:19 +0530

    Update map_renderer.py

--- map_renderer.py
+++ map_renderer.py
@@ -264,7 +264,7 @@ def render_map(
     # SOURCE
     # =============================
     fig.text(
-        0.035, 0.030, SOURCE_TEXT,
+        0.04, 0.033, SOURCE_TEXT,
         ha="left", va="bottom",
         fontsize=9, fontweight="bold",
         color="#555555"
@@ -294,4 +294,5 @@ def render_map(
     plt.close()
 
     print("India heatmap created successfully")
+
 
